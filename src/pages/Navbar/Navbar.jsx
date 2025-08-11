a7bf14e230e71aaa1eb5ad306f81c4bf51536c8b
shaker11Batch <saenterprise67811@gmail.com>
2025-08-11 18:47:01 +0600

    navbar responsive

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -1,21 +1,46 @@
-import React from 'react';
-import './Navbar.css'
+import React, { useState } from "react";
+import "./Navbar.css";
+import { Menu, X } from "lucide-react";
 
 const Navbar = () => {
-    return (
-        <div>
-            <nav className='navbar'>
-                <a href="#" className='logo'>Protfolio.</a>
-                <ul>
-                <li className='active'><a href="£">Home</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Portfolio</a></li>
-                <li><a href="">Service</a></li>
-                <li><a href="">Contact</a></li>
-                </ul>
-            </nav> 
-        </div>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+     
+      <a href="#" className="logo">
+        Portfolio.
+      </a>
+
+    
+      <ul className={`navbar-li ${isOpen ? "open" : ""}`}>
+        <li className="active">
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Portfolio</a>
+        </li>
+        <li>
+          <a href="#">Service</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+
+      {/* Menu Icon */}
+      <div onClick={handleMenu} className="menu">
+        {isOpen ? <X size={32} /> : <Menu size={32} />}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
